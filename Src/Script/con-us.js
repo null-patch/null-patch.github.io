b741b0b080aef977a4515bc428777fbdf464b334
Null-Patch <ashleyman004@gmail.com>
2021-12-19 00:04:28 +0600

    The Contract us page is complete

---
+++ Src/Script/con-us.js
@@ -0,0 +1,48 @@
+const icon = document.getElementById("icon");
+icon.onclick = function () {
+    document.body.classList.toggle("dark-theme");
+    if (document.body.classList.contains("dark-theme")) {
+        icon.src = "../../Assets/sun.png";
+    } else {
+        icon.src = "../../Assets/moon.png";
+    }
+}
+
+
+window.addEventListener("DOMContentLoaded", function () {
+    var form = document.getElementById("my-form");
+    var status = document.getElementById("status");
+
+    function success() {
+        form.reset();
+        status.classList.add("success");
+        status.innerHTML = "Thanks!";
+    }
+
+    function error() {
+        status.classList.add("error");
+        status.innerHTML = "Oops! There was a problem.";
+    }
+
+
+    form.addEventListener("submit", function (ev) {
+        ev.preventDefault();
+        var data = new FormData(form);
+        ajax(form.method, form.action, data, success, error);
+    });
+});
+
+function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState !== XMLHttpRequest.DONE) return;
+        if (xhr.status === 200) {
+            success(xhr.response, xhr.responseType);
+        } else {
+            error(xhr.status, xhr.response, xhr.responseType);
+        }
+    };
+    xhr.send(data);
+}
