36eaddb54a2975fe83de5bf047dafb38978ae5af
Null-Patch <ashleyman004@gmail.com>
2022-01-19 03:25:37 +0600

    idk what to add

--- Src/Script/con-us.js
+++ Src/Script/con-us.js
@@ -1,7 +1,16 @@
 var logo = document.getElementById('logo');
 logo.onclick = function(){location.reload()};
 
-document.addEventListener('contextmenu', event => event.preventDefault());
+window.addEventListener('contextmenu', function(event){
+    event.preventDefault();
+    var contextElement = document.getElementById("contex-menu")
+    contextElement.style.left = event.pageX + "px";
+    contextElement.style.top = event.pageY + "px";
+    contextElement.classList.add("active");
+});
+window.addEventListener("click", function(){
+    document.getElementById("contex-menu").classList.remove("active")
+});
 
 let darkMode = localStorage.getItem('darkMode');
 const darkModeToggle = document.querySelector('#dark-mode-toggle');
