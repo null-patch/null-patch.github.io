b4eed5d6b9cd43d465f36e654e005afdc2a40a54
Null-Patch <ashleyman004@gmail.com>
2022-01-26 00:03:00 +0600

    Update 404.js

--- Src/Script/404.js
+++ Src/Script/404.js
@@ -6,3 +6,10 @@ window.onmousemove = function(e){
     container.style.backgroundPositionY = y + 'px';
 };
 document.addEventListener('contextmenu', event => event.preventDefault());
+
+window.ondeviceorientation = function(f){
+    var x = f.clientX,
+        y = f.clientY;
+    container.style.backgroundPositionX = x + 'px';
+    container.style.backgroundPositionX = y + 'px';
+};
